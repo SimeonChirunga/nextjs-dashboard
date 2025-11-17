f0e3be96e8022a48b3cdeb62d16039f547cbe713
simeonchirunga <schirunga1@byupathway.edu>
2025-11-17 23:06:40 +0200

    testw

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -111,8 +111,13 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-    // throw new Error('Failed to Delete Invoice');
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.error(error);
+    return { message: `Database Error: Failed to Delete Invoice. ${error}` };
+  }
+
   revalidatePath('/dashboard/invoices');
   return { message: 'Deleted Invoice.' };
 }
