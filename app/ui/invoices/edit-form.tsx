9a81b10f2c6ebdf75033e9e7dd5ea339e807af02
simeonchirunga <schirunga1@byupathway.edu>
2025-11-17 21:44:41 +0200

    weekly commit: Update page metadata and sign-out form

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
@@ -10,6 +11,10 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+
+
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,8 +22,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  return (
-    <form>
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+ 
+  return <form action={formAction}> 
+    
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -118,6 +127,9 @@ export default function EditInvoiceForm({
         </Link>
         <Button type="submit">Edit Invoice</Button>
       </div>
+    
     </form>
-  );
-}
+  };
+  
+
+
