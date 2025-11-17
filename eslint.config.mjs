0b3490761c303067e92a504029a88464f41634a5
simeonchirunga <schirunga1@byupathway.edu>
2025-11-17 22:35:09 +0200

    again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,24 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+
 import js from "@eslint/js";
 import next from "@next/eslint-plugin-next";
-import { defineConfig } from "eslint/config";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
+export default [
+  // Next.js core web vitals configuration
+  {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    ...js.configs.recommended,
     plugins: {
       next
     },
     rules: {
-        ...next.configs.recommended.rules,
-        extends: compat.extends("next/core-web-vitals"),
+      ...next.configs.recommended.rules,
+      ...next.configs['core-web-vitals'].rules,
     }
-}]);
+  },
+  
+  // Additional 
+  {
+    rules: {
+      // your custom rules here
+    }
+  }
+];
